ae571485f4fa7988a7c52b353c038ab792308984
junios <junios@naver.com>
2024-01-09 16:49:51 +0900

    숫자 더하기 클라이언트

--- client.cpp
+++ client.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <WinSock2.h>
+#include <string>
 
 #pragma comment(lib, "ws2_32")
 
@@ -33,14 +34,86 @@ int main()
 		exit(-1);
 	}
 
-	char RecvBuffer[1024] = { 0, };
-	int RecvByte = recv(ServerSocket, RecvBuffer, sizeof(RecvBuffer), 0);
+	while (true)
+	{
+		char RecvBuffer[1024] = { 0, };
+		int RecvByte = recv(ServerSocket, RecvBuffer, sizeof(RecvBuffer), 0);
 
-	cout << "Server Sent " << RecvBuffer << endl;
+		cout << "Server Sent " << RecvBuffer << endl;
 
-	int SentByte = send(ServerSocket, RecvBuffer, (int)strlen(RecvBuffer), 0);
+		string Data = RecvBuffer;
+		size_t OperatorPosition = Data.find('+');
+		if (OperatorPosition == string::npos)
+		{
+			OperatorPosition = Data.find('-');
+			if (OperatorPosition == string::npos)
+			{
+				OperatorPosition = Data.find('*');
+				if (OperatorPosition == string::npos)
+				{
+					OperatorPosition = Data.find('/');
+					if (OperatorPosition == string::npos)
+					{
+						OperatorPosition = Data.find('%');
+					}
+				}
+			}
+		}
 
-	cout << "Send Data" << endl;
+		string FirstNumberString = Data.substr(0, OperatorPosition);
+		string SecondNumberString = Data.substr(OperatorPosition + 1,
+			Data.length() - OperatorPosition - 1);
+
+		cout << FirstNumberString << endl;
+		cout << SecondNumberString << endl;
+
+		int FirstNumber = stoi(FirstNumberString);
+		int SecondNumber = stoi(SecondNumberString);
+		int ResultNumber = 0;
+		OperatorPosition = Data.find('+');
+		if (OperatorPosition == string::npos)
+		{
+			OperatorPosition = Data.find('-');
+			if (OperatorPosition == string::npos)
+			{
+				OperatorPosition = Data.find('*');
+				if (OperatorPosition == string::npos)
+				{
+					OperatorPosition = Data.find('/');
+					if (OperatorPosition == string::npos)
+					{
+						OperatorPosition = Data.find('%');
+						ResultNumber = FirstNumber % SecondNumber;
+					}
+					else
+					{
+						ResultNumber = FirstNumber / +SecondNumber;
+					}
+				}
+				else
+				{
+					ResultNumber = FirstNumber * SecondNumber;
+				}
+			}
+			else
+			{
+				ResultNumber = FirstNumber - SecondNumber;
+			}
+		}
+		else
+		{
+			ResultNumber = FirstNumber + SecondNumber;
+		}
+
+
+
+		char Buffer[1024] = { 0, };
+		sprintf_s(Buffer, 1024, "%d", ResultNumber);
+
+		int SentByte = send(ServerSocket, Buffer, (int)(strlen(Buffer) + 1), 0);
+
+		cout << Buffer << endl;
+	}
 
 	closesocket(ServerSocket);
 
